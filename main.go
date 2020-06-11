74e5ab2cf9604a966c7eb09658310da1bcda6e4b
Junya Terada <chapico01@gmail.com>
2020-06-11 23:01:25 +0900

    add httphandle

--- main.go
+++ main.go
@@ -1,32 +1,18 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"github.com/joho/godotenv"
-	"log"
 	"net/http"
-	"os"
 )
 
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	url := os.Getenv("MY_SLACK_URL")
-	param := "payload={\"text\": \"ハゲハゲ\"}"
-	req, err := http.NewRequest("POST", url, bytes.NewBufferString(param))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(res)
-
+func HandleGithubPost(w http.ResponseWriter, r *http.Request){
+	fmt.Println("githubからきたよ")
+	sendMessage()
 }
+
+func main() {
+	http.HandleFunc("/mention", HandleGithubPost)
+	http.ListenAndServe(":8080", nil)
+}
+
+
